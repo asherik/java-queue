a2b60c054809254b48a4d372ebff1c7a21f78e94
Ашот агабеков <agabekov.aa@esphere.ru>
2023-02-18 18:13:57 +0300

    рефакторинг

--- src/main/java/ru/org/Queue.java
+++ src/main/java/ru/org/Queue.java
@@ -4,41 +4,58 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class Queue {
+
     //Глобальное хранение очереди
-    private static BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
+    private static final BlockingQueue<String> queueGlobal = new ArrayBlockingQueue<>(10);
 
     public static void main(String[] args) {
         // Работа продюсера
-        Thread producerThread = new Thread(() -> {
-            while (true) {
-                try {
-                    // Добавление данных в очередь
-                    String data = "Время " + System.currentTimeMillis();
-                    queue.put(data);
-                    System.out.println("Продюсер добавил данные в очередь: " + data);
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        Thread producerThread = producerThread();
 
         // Работа консюмера
-        Thread consumerThread = new Thread(() -> {
-            while (true) {
-                try {
-                    // Чтение из очереди
-                    String data = queue.take();
-                    System.out.println("Консьюмер прочел из очереди: " + data);
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        Thread consumerThread = consumerThread();
 
         // Запуск потоков
         producerThread.start();
         consumerThread.start();
+    }
+
+    private static Thread producerThread() {
+        return new Thread(() -> {
+            while (true) {
+                try {
+                    // Добавление данных в очередь
+                    producerAddInQueue();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private static Thread consumerThread() {
+        return new Thread(() -> {
+            while (true) {
+                try {
+                    // Чтение из очереди
+                    producerReadFromQueue();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private static void producerReadFromQueue() throws InterruptedException {
+        String data = queueGlobal.take();
+        System.out.println("Консьюмер прочел из очереди: " + data);
+        Thread.sleep(2000);
+    }
+
+    private static void producerAddInQueue() throws InterruptedException {
+        String data = "Время " + System.currentTimeMillis();
+        queueGlobal.put(data);
+        System.out.println("Продюсер добавил данные в очередь: " + data);
+        Thread.sleep(2000);
     }
 }
